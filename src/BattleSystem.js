43324fa76a85e0e0f3c09b0b27ff7b454b507ffa
Drake Wilcox <drakewilcox@gmail.com>
2020-04-15 15:10:30 -0700

    add content for battlesystem as a starting point for future code.
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- src/BattleSystem.js
+++ src/BattleSystem.js
@@ -17,3 +17,16 @@ const changeState = (prop) => {
     });
   };
 };
+
+intialWeaponValues = {damage: 0}
+
+const rubberDuck = storeState(intialWeaponValues);
+const hammer = storeState(intialWeaponValues);
+const garlicBreath = storeState(intialWeaponValues); 
+
+initialBossValues = {name: "", health: 100, damage: 0}
+
+const sauceBoss = storeState(initialBossValues);
+const irritatedGoose = storeState(initialBossValues);
+const sleepwalkingNeighbor = storeState(initialBossValues);
+const poorlyWrittenCode = storeState(initialBossValues);
