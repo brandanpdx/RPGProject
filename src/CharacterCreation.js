e3b493e532d06eac940f2ec6c6c7a6124016ab9b
Drake Wilcox <drakewilcox@gmail.com>
2020-04-15 10:56:07 -0700

    add initial characters.
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- src/CharacterCreation.js
+++ src/CharacterCreation.js
@@ -18,8 +18,18 @@ const changeState = (prop) => {
   };
 };
 
-const initialCharacterValues = {name: "", health: 100, strength: 30, level: 1, coins: 50};
+const initialCharacterValues = {name: "", health: 0, strength: 0, level: 0, coins: 0};
 const characterMaster = storeState(initialCharacterValues); 
-const newPlayer = changeState("name");
-const currentPlayer = newPlayer("Dracobian");
-const characterObj = characterMaster(currentPlayer);
+const newPlayer = changeState("name")("health")("strength")("level")("coins");
+
+const player1 = newPlayer("Dracobian")(100)(10)(1)(150);
+export const player1Obj = characterMaster(player1);
+
+const player2 = newPlayer("Mr. Miyagi")(100)(80)(1)(25);
+export const player2Obj = characterMaster(player2);
+
+const player3 = newPlayer("Todders")(75)(30)(1)(150);
+export const player3Obj = characterMaster(player3);
+
+const player4 = newPlayer("Epicodbro")(100)(50)(1)(150);
+export const player4Obj = characterMaster(player4);
