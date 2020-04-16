64c040fc00b0ecaa9a39cdd182cba7a98f65c8ed
Drake Wilcox <drakewilcox@gmail.com>
2020-04-16 10:42:02 -0700

    confirm test passes for creation of new boss object.
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,10 +1,10 @@
-import {newName, gameMaster} from './../src/CharacterCreation.js';
+import {newCharacterName, characterMaster, bossMaster, newBossName} from './../src/SauceQuest.js';
 
 describe('player', () => {
 
   test('should correctly set player name and initial values for strength, coins, level, name, and health.', () => {
-    const newPlayer = newName("Dracobian");
-    const newPlayerObj = gameMaster(newPlayer);
+    const newPlayer = newCharacterName("Dracobian");
+    const newPlayerObj = characterMaster(newPlayer);
     expect(newPlayerObj.strength).toEqual(30);
     expect(newPlayerObj.coins).toEqual(100);
     expect(newPlayerObj.level).toEqual(1);
@@ -12,3 +12,14 @@ describe('player', () => {
     expect(newPlayerObj.health).toEqual(100);
   });
 });
+
+describe('boss' , () => {
+  test('should correctly set boss name and initial values', () => {
+    const newBoss = newBossName("Sir. Racha"); 
+    const newBossObj = bossMaster(newBoss);
+    expect(newBossObj.name).toEqual("Sir. Racha");
+    expect(newBossObj.health).toEqual(100);
+    expect(newBossObj.damage).toEqual(0);
+    expect(newBossObj.special).toEqual(0);
+  })
+})
